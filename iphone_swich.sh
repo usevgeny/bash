45472f524e6cf89a1e8e34cc7401e5498cdaaa88
Evgeny <evgeny.rfi@example.com>
2020-10-02 00:18:14 +0200

    without backup , still not working

--- iphone_swich.sh
+++ iphone_swich.sh
@@ -25,40 +25,40 @@ log () {
 }
 
 #before we create a substitution to an existing file,it it necessary to backup existing ones
-backup_network() {
-	local NETWORK_FILE="$(/etc/sysconfig/network)"
-	if [[ -f "${NETWORK_FILE}" ]]
-	then
-		local BACKUP_NETWORK="/var/tmp/$(basename ${NETWORK_FILE}).$(date +%F-%N)"
-		log "Backing up ${NETWORK_FILE} to ${BACKUP_FILE}."
+#backup_network() {
+#	local NETWORK_FILE="$(/etc/sysconfig/network)"
+#	if [[ -f "${NETWORK_FILE}" ]]
+#	then
+#		local BACKUP_NETWORK="/var/tmp/$(basename ${NETWORK_FILE}).$(date +%F-%N)"
+#		log "Backing up ${NETWORK_FILE} to ${BACKUP_FILE}."
 		#the exit status of the functioin wille be the exit status of the cp command
-		cp -p ${NETWORK_FILE} ${BACKUP_NETWORK}
-	else
-		#the file does not exist. Exit with no zero status
-		return 1
-	fi
+#		cp -p ${NETWORK_FILE} ${BACKUP_NETWORK}
+#	else
+#		#the file does not exist. Exit with no zero status
+#		return 1
+#	fi
+#
+#}
 
-}
 
+#backup_network_scripts() {
+#
+#	local NETWORK_SCRIPTS="$/etc/sysconfig/network-scripts"
+#	if [[ -f "${NETWORK_SCRIPTS}" ]]
+#	then
+#		local NETWORK_SCRIPTS_BACKUP="/var/tmp/$(basename ${NETWORK_SCRIPTS}).$(date +%F-%N)"
+#		log "Backing up ${NETWORK_SCRIPTS} to ${NETWORK_SCRIPTS_BACKUP}."
+#		#the exit status of the functioin wille be the exit status of the cp command
+#		cp -r -p ${NETWORK_SCRIPTS} ${NETWORK_SCRIPTS_BACKUP}
+#
+#	else
+#		#the dir doesnot exist. Exit with no zero status
+#		return 1
+#
+#		
+#	fi
 
-backup_network_scripts() {
-
-	local NETWORK_SCRIPTS="$/etc/sysconfig/network-scripts"
-	if [[ -f "${NETWORK_SCRIPTS}" ]]
-	then
-		local NETWORK_SCRIPTS_BACKUP="/var/tmp/$(basename ${NETWORK_SCRIPTS}).$(date +%F-%N)"
-		log "Backing up ${NETWORK_SCRIPTS} to ${NETWORK_SCRIPTS_BACKUP}."
-		#the exit status of the functioin wille be the exit status of the cp command
-		cp -r -p ${NETWORK_SCRIPTS} ${NETWORK_SCRIPTS_BACKUP}
-
-	else
-		#the dir doesnot exist. Exit with no zero status
-		return 1
-
-		
-	fi
-
-}
+#}
 
 report() {
 	#make a decisoin based  on the exit status of the function
@@ -80,31 +80,33 @@ echo "GATEWAY=${NEW_GATEWAY}" >> /etc/sysconfig/network
 }
 
 
+NETWORK_SCRIPT_FNAME="$(ls /etc/sysconfig/network-scripts | head -n1)"
 changing_nsfn() {
 
 #Applying changes for the network-scripts
 
 #the name of the nteworkscript could be different so it will be applied accordingly. head -n1 is to chose the first file in the list
 
-NETWORK_SCRIPT_FNAME="$(ls /etc/sysconfig/network-scripts | head -n1)"
 
-echo 'TYPE=Ethernet' >> /etc/sysconfig/network-scripts/$(basename ${NETWORK_SCRIPT_FNAME})
-echo 'PROXY_METHOD=none' >> /etc/sysconfig/network-scripts/$(basename ${NETWORK_SCRIPT_FNAME})
-echo 'BROWSER_ONLY=no' >> /etc/sysconfig/network-scripts/$(basename ${NETWORK_SCRIPT_FNAME})
-echo 'BOOTPROTO=none' >> /etc/sysconfig/network-scripts/$(basename ${NETWORK_SCRIPT_FNAME})
-echo 'DEFROUTE=yes' >> /etc/sysconfig/network-scripts/$(basename ${NETWORK_SCRIPT_FNAME})
-echo 'IPV4_FAILURE_FATAL=no' >> /etc/sysconfig/network-scripts/$(basename ${NETWORK_SCRIPT_FNAME})
-echo "IPADDR=172.20.10.2" >> /etc/sysconfig/network-scripts/$(basename ${NETWORK_SCRIPT_FNAME})
-echo 'NETMASK=255.255.255.0' >> /etc/sysconfig/network-scripts/$(basename ${NETWORK_SCRIPT_FNAME})
-echo 'IPV6INIT=no' >> /etc/sysconfig/network-scripts/$(basename ${NETWORK_SCRIPT_FNAME})
-echo '#IPV6_AUTOCONF=yes' >> /etc/sysconfig/network-scripts/$(basename ${NETWORK_SCRIPT_FNAME})
-echo '#IPV6_DEFROUTE=yes' >> /etc/sysconfig/network-scripts/$(basename ${NETWORK_SCRIPT_FNAME})
-echo '#IPV6_FAILURE_FATAL=no' >> /etc/sysconfig/network-scripts/$(basename ${NETWORK_SCRIPT_FNAME})
-echo '#IPV6_ADDR_GEN_MODE=stable-privacy' >> /etc/sysconfig/network-scripts/$(basename ${NETWORK_SCRIPT_FNAME})
-echo 'NAME=enp0s3' >> /etc/sysconfig/network-scripts/$(basename ${NETWORK_SCRIPT_FNAME})
-echo 'UUID=141a5b86-f665-4bbc-8678-d96b6df0cc98' >> /etc/sysconfig/network-scripts/$(basename ${NETWORK_SCRIPT_FNAME})
-echo 'DEVICE=enp0s3' >> /etc/sysconfig/network-scripts/$(basename ${NETWORK_SCRIPT_FNAME})
-echo 'ONBOOT=yes' >> /etc/sysconfig/network-scripts/$(basename ${NETWORK_SCRIPT_FNAME})
+
+echo 'TYPE=Ethernet' >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo 'PROXY_METHOD=none' >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo 'BROWSER_ONLY=no' >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo 'BOOTPROTO=none' >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo 'DEFROUTE=yes' >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo 'IPV4_FAILURE_FATAL=no' >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo "IPADDR=172.20.10.2" >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo 'NETMASK=255.255.255.0' >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo 'IPV6INIT=no' >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo '#IPV6_AUTOCONF=yes' >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo '#IPV6_DEFROUTE=yes' >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo '#IPV6_FAILURE_FATAL=no' >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo '#IPV6_ADDR_GEN_MODE=stable-privacy' >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo 'NAME=enp0s3' >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo 'UUID=141a5b86-f665-4bbc-8678-d96b6df0cc98' >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo 'DEVICE=enp0s3' >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+echo 'ONBOOT=yes' >> /etc/sysconfig/network-scripts/ifcfg-enp0s3
+
 
 }
 
@@ -119,13 +121,13 @@ esac
 done
 
 
-log 'backing up network file '
-backup_network
-report
+#log 'backing up network file '
+#backup_network
+#report
 
-log 'backing up network scripts '
-backup_network_scripts
-report
+#log 'backing up network scripts '
+#backup_network_scripts
+#report
 
 
 #let user enter new fixed  ip
